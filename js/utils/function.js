b0eb6d6e61afb840046c92e8a7e973b1b53ba070
Zzx-LiU6 <zzxuan1007@gmail.com>
2026-07-12 17:43:41 +0800

    Create function.js

---
+++ js/utils/function.js
@@ -0,0 +1,9 @@
+// ========== 函数工具 ==========
+
+function debounce(fn, delay) {
+  let timer = null;
+  return function(...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), delay);
+  };
+}
